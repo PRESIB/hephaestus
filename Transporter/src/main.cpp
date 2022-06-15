5a762e08a5297d7b5472a605bd6025952b10f37e
Nuno Fernandes <nuno.lopes_fernandes.ext@siemens-energy.com>
2022-06-15 23:52:37 +0100

    revert changes

--- Transporter/src/main.cpp
+++ Transporter/src/main.cpp
@@ -12,7 +12,7 @@ char *NET_COMMAND_END_SERVICE = (char *)"e";
 #define DEVICE_COMMAND_READY "ready"
 #define DEVICE_COMMAND_END_SERVICE "end_service"
 
-const char *HOLON_ID = "rht-001";
+const String HOLON_ID = "rht-001";
 
 // WiFi
 const char *ssid = "MEO-9ABB30";     // The SSID (name) of the Wi-Fi network you want to connect to
@@ -20,8 +20,8 @@ const char *password = "4ff01eb05e"; // The password of the Wi-Fi network
 
 // MQTT Broker
 const char *mqtt_broker = "test.mosquitto.org";
-const char *netTopic = "com/nfriacowboy/presib/holon/mqtt/net/";
-const char *deviceTopic = "com/nfriacowboy/presib/holon/mqtt/device/";
+const char *netTopic = "com/nfriacowboy/presib/holon/mqtt/net/rht-001";
+const char *deviceTopic = "com/nfriacowboy/presib/holon/mqtt/device/rht-001";
 const char *systemTopic = "com/nfriacowboy/presib/hermes/management/system";
 
 const int mqtt_port = 1883;
@@ -32,12 +32,10 @@ PubSubClient mqttClient(espClient);
 void publish(char *message)
 {
   digitalWrite(ONBOARD_LED, HIGH);
-  int tl = strlen(netTopic) + strlen(HOLON_ID);
-  char topic[tl + 1];
-  strcpy(topic, netTopic);
-  strcat(topic, HOLON_ID);
+  Serial.println("sending to: ");
+  Serial.println(netTopic);
 
-  if (mqttClient.publish(topic, message))
+  if (mqttClient.publish(netTopic, message))
   {
     Serial.println("message sent");
   }
@@ -65,7 +63,7 @@ void callback(char *topic, byte *payload, unsigned int length)
     strncpy(message, (char *)payload, length);
     message[length] = '\0';
 
-    Serial.printf("%s", message);
+    Serial.println(String(message));
     Serial.println("-----------------------");
 
     digitalWrite(ONBOARD_LED, LOW);
@@ -73,20 +71,28 @@ void callback(char *topic, byte *payload, unsigned int length)
     if (strcmp(DEVICE_COMMAND_READY, message) == 0)
     {
       digitalWrite(ONBOARD_LED, HIGH);
+      Serial.println("ready command received");
+      Serial.println("sending ready command to net");
       publish(NET_COMMAND_READY);
       digitalWrite(ONBOARD_LED, LOW);
     }
     else if (strcmp(DEVICE_COMMAND_END_SERVICE, message) == 0)
     {
       digitalWrite(ONBOARD_LED, HIGH);
+      Serial.print("end service command received");
       publish(NET_COMMAND_END_SERVICE);
       digitalWrite(ONBOARD_LED, LOW);
     }
     else if (strcmp(DEVICE_COMMAND_START_SERVICE, message) == 0)
     {
       digitalWrite(ONBOARD_LED, HIGH);
+      Serial.print("start service command received");
       publish(NET_COMMAND_START_SERVICE);
       digitalWrite(ONBOARD_LED, LOW);
+    }
+    else
+    {
+      Serial.println("Is not ready command");
     }
   }
 }
@@ -97,12 +103,14 @@ void setupMqqtClient()
   mqttClient.setCallback(callback);
   while (!mqttClient.connected())
   {
-
-    char client_id[20];
-    strcpy(client_id, "rh::");
-    strcat(client_id, WiFi.macAddress().c_str());
-
-    if (!mqttClient.connect(client_id))
+    String client_id = "rh::";
+    client_id += String(WiFi.macAddress());
+    Serial.printf("The client %s connects to the public mqtt broker\n", client_id.c_str());
+    if (mqttClient.connect(client_id.c_str()))
+    {
+      Serial.println("Public emqx mqtt broker connected");
+    }
+    else
     {
       digitalWrite(ONBOARD_LED, HIGH);
       Serial.print("failed with state ");
@@ -113,19 +121,9 @@ void setupMqqtClient()
   }
 
   // publish and subscribe
-  char message[20] = "Hi from ";
-  strcpy(message, "Hi from ");
-  strcat(message, HOLON_ID);
-
-  int dl = strlen(deviceTopic);
-  char device[dl + 1];
-  strcpy(device, deviceTopic);
-  strcat(device, HOLON_ID);
-
-  // mqttClient.publish(systemTopic, message);
-  //  Serial.printf("Subscribe to %s", device);
-  //  mqttClient.subscribe(device);
-  //   publish(NET_COMMAND_READY);
+  mqttClient.publish(systemTopic, String("Hi from " + HOLON_ID).c_str());
+  mqttClient.subscribe(deviceTopic);
+  publish(NET_COMMAND_READY);
 }
 
 void setup()
@@ -134,15 +132,19 @@ void setup()
   digitalWrite(ONBOARD_LED, HIGH);
   Serial.begin(115200); // Start the Serial communication to send messages to the computer
   delay(10);
+  Serial.println('\n');
   digitalWrite(ONBOARD_LED, LOW);
 
   WiFi.begin(ssid, password); // Connect to the network
+  Serial.print("Connecting to ");
+  Serial.print(ssid);
 
   while (WiFi.status() != WL_CONNECTED)
   { // Wait for the Wi-Fi to connect
     digitalWrite(ONBOARD_LED, HIGH);
     delay(500);
     digitalWrite(ONBOARD_LED, LOW);
+    Serial.print('.');
   }
 
   Serial.println('\n');
